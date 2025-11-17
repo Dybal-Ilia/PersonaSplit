32181de8b74641b020ebf6e6c10c37b607e6673a
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-17 08:01:31 +0100

    refactor: enhance debate command handling and preset selection logic

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,17 @@
 import asyncio
-import logging
-from pathlib import Path
-import yaml
 from dotenv import load_dotenv
 from os import getenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from langchain_core.messages import SystemMessage
-from src.core.agents.agent import Agent
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name=__name__)
+from aiogram.types import ReplyKeyboardRemove
+from aiogram import F
+from aiogram.fsm.context import FSMContext
+from aiogram.enums import ChatAction
+from src.utils.logger import logger
+from src.utils.loaders import load_yaml
+from src.core.agents.orchestration import GraphFactory
+from src.core.schemas.state import ChatState
 
 load_dotenv()
 
@@ -18,42 +19,68 @@ BOT_TOKEN = getenv('BOT_TOKEN')
 PRESETS_PATH = getenv('PRESETS_PATH')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+presets = load_yaml(PRESETS_PATH)
+TELEGRAM_MAX_LENGTH = 4096
 
 @dp.message(Command("debate"))
-async def cmd_start(message: types.Message):
-    user_topic = message.text
-    rationalist = Agent("rationalist")
-    humanist = Agent("humanist")
-    debate_history = [SystemMessage(f"Orchestrator said: You are to debate on {user_topic}")]
-    rationalist_response = rationalist.run(topic=user_topic, messages=debate_history)
-    
-    await message.answer(rationalist.history[-1])
+async def cmd_start(message: types.Message, state:FSMContext):
+    topic = message.text.split(maxsplit=1)[-1]
+    user_data = await state.get_data()
+    preset = user_data.get("selected_preset", "classic")
+    try:
+        agents_list = presets[preset]["agents"]
+    except KeyError:
+        await message.answer("An error occurred. Preset is not found. Switching to 'classic'.")
+        agents_list = presets["classic"]["agents"]
+    await message.answer(f"Got You! (Preset: {preset})\nPreparing Debators...")
+    graph = GraphFactory(agents_list=agents_list)
+    app = graph.build_graph()
+    initial_state = ChatState(
+        topic=topic,
+        debators=agents_list
+        )
 
-    debate_history.append(rationalist_response["messages"][-1])
+    async for event in app.astream(initial_state, config={"recursion_limit": 100}):
+        node_name, patch = list(event.items())[0]
+        if node_name == "Orchestrator" or node_name == "HistoryManager":
+            continue
 
-    humanist_response = humanist.run(topic=user_topic, messages=debate_history)
-    await message.answer(humanist.history[-1])
+        if "history_patch" in patch and patch["history_patch"]:
+            await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+            response_message = patch["history_patch"]
+            persona_name = node_name.capitalize()
+            full_text = f"**{persona_name}**\n{response_message.content.split(':', maxsplit=1)[-1]}"
+            if len(full_text) <= TELEGRAM_MAX_LENGTH:
+                await message.answer(full_text)
+            else:
+                for i in range(0, len(full_text), TELEGRAM_MAX_LENGTH):
+                    chunk = full_text[i : i + TELEGRAM_MAX_LENGTH]
+                    await message.answer(chunk)
+        elif node_name == "Judge":
+            judge_decision = patch["judge_decision"]
 
-    debate_history.append(humanist_response["messages"][-1])
-
+            await message.answer(f"**Judges Verdict:**\n{judge_decision.content}")
 
 @dp.message(Command("preset"))
 async def cmd_preset(message: types.Message):
-    with open(PRESETS_PATH, "r") as f:
-        presets = yaml.safe_load(f)
+
+    presets = load_yaml(path=PRESETS_PATH)
     builder = ReplyKeyboardBuilder()
     for preset in presets.keys():
         builder.button(text=preset, callback_data=preset)
-    builder.adjust(3)
-    await message.answer("Some text here", reply_markup=builder.as_markup())
+    builder.adjust(4)
+    await message.answer("Choose one of available presets below",
+                         reply_markup=builder.as_markup(resize_keyboard=True))
 
+@dp.message(F.text.in_(presets.keys()))
+async def preset_chosen(message: types.Message, state:FSMContext):
+    chosen = message.text
+    await state.update_data(selected_preset=chosen)
 
-@dp.message()
-async def cmd_echo(message:types.Message):
-    with open(PRESETS_PATH, "r") as f:
-        presets = yaml.safe_load(f)
-    if message.text in presets.keys():
-        await message.answer(f"Curent debators: {presets[message.text]["agents"]}")
+    await message.answer(
+        f"Chosen preset is {chosen}\nThe debators are: {", ".join(presets[chosen]["agents"])}",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
 async def main():
