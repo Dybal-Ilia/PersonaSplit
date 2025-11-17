5df967e988cdd8a695041ba49a39eddb495d6484
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-17 08:01:48 +0100

    refactor: update ChatState model to include additional fields and improve structure

--- src/core/schemas/state.py
+++ src/core/schemas/state.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, Field
-from typing import Annotated, List
+from typing import Annotated, List, Optional
 from langchain_core.messages import BaseMessage
 from langgraph.graph import add_messages
 
 
-class AgentState(BaseModel):
+class ChatState(BaseModel):
     topic: str = Field(..., description="The topic provided by a user's input")
-    messages: Annotated[List[BaseMessage], add_messages] = Field(..., description="The history of the debates")
-
-
+    debators: List[str]
+    history: Annotated[List[BaseMessage], add_messages] = []
+    recent_history: List[BaseMessage] = []
+    history_patch: Optional[BaseMessage] = None
+    last_speaker: str = None
+    next_speaker:str = None
+    judge_decision: Optional[BaseMessage] = None
+    replices_counter: int = 0
