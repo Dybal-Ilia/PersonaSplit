90f78cd2e5526fd5c255133edd8cb62b20c8041b
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-17 08:00:41 +0100

    refactor: restructure agent classes and improve response handling

--- src/core/agents/agent.py
+++ src/core/agents/agent.py
@@ -1,94 +1,114 @@
 from typing import Annotated, List
-from pydantic import BaseModel, Field
+from langchain_groq.chat_models import ChatGroq
 from langchain_ollama.chat_models import ChatOllama
-from  langgraph.graph import StateGraph, END, START
+from langgraph.graph import StateGraph, END, START
 from langgraph.graph.message import add_messages
 from langchain_core.messages import AIMessage, BaseMessage
 from langchain_core.prompts import ChatPromptTemplate
-import logging
-import yaml
 from dotenv import load_dotenv
 import os
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name=__name__)
-
+from src.utils.logger import logger
+from src.utils.loaders import load_yaml
+from src.core.schemas.state import ChatState
+from abc import abstractmethod, ABC
 load_dotenv()
 
 prompts_path = os.getenv("PROMPTS_PATH")
+API_KEY = os.getenv("GROQ_API_KEY")
 
 
-class AgentState(BaseModel):
-    topic: str = Field(..., description="The topic provided by a user's input")
-    messages: Annotated[List[BaseMessage], add_messages] = Field(..., description="The history of the debates")
 
 
-class Agent:
-    def __init__(self, name:str, model:str = 'llama3.1'):
+class BaseAgent(ABC):
+    def __init__(self, name:str, model:str="llama-3.1-8b-instant"):
         self.name = name.strip().lower()
-        self.llm = ChatOllama(model=model, temperature=0.5)
-        self.prompt = self._get_prompt()
-        self.history: List[str] = []
-        self.graph = self._build_graph()
-    
-    def _get_prompt(self):
-        try:
-            with open(prompts_path, "r") as f:
-                all_prompts = yaml.safe_load(f)
-                persona_prompt = all_prompts[self.name.strip().lower()]["system_prompt"]
-                llm_prompt = ChatPromptTemplate.from_template(persona_prompt)
-                logger.info(f"System prompt for {self.name} loaded successfully")
-        except FileNotFoundError:
-            logger.error(f"Prompts file does not exist")
-            exit()
-        except Exception as e:
-            logger.error(f"An error occurred while loading prompt: {str(e)}")
-            exit()
+        self.llm = ChatGroq(model=model,
+                            api_key=API_KEY,
+                            max_tokens=1024)
+        self.prompt = ChatPromptTemplate.from_template(load_yaml(prompts_path)[self.name]["system_prompt"])
+        self.chain = self.prompt | self.llm
 
-        return llm_prompt
 
-    def _build_graph(self):
-        graph = StateGraph(AgentState)
-        graph.add_node("LLM_CALL", self._llm_call)
-        graph.add_node("SUMMARIZER", self._summarize)
-        graph.add_edge(START, "LLM_CALL")
-        graph.add_edge("LLM_CALL", "SUMMARIZER")
-        graph.add_edge("SUMMARIZER", END)
-        return graph.compile()
+    @abstractmethod
+    async def run(self, state:ChatState):
+        raise NotImplementedError
+
+class Persona(BaseAgent):
     
-    def _llm_call(self, state: AgentState) -> AgentState:
-        """This node is responsible for llm calling and generating a response"""
-        chain = self.prompt | self.llm
-        response = chain.invoke({
+    async def run(self, state:ChatState):
+        logger.info(f"{self.name} called to the debates")
+        response: AIMessage = await self.chain.ainvoke({
             "topic": state.topic,
-            "messages": state.messages,
-            "history": self.history
+            "history": state.history,
+            "recent_history": state.recent_history
         })
-        content = response.content.strip()
-        self.history.append(content)
-        return state
-
-    def _summarize(self, state: AgentState) -> AgentState:
-        """This node is responsible for summarizing messages"""
-        prompt_template = """ You are an AI assistant resposible for text summarization.
-        You are given message to be summarized: {history}. Your goal is to slightly summarize the message, yet keep it informative.
-        Try to higlight most important things, but if you decide that something can be potentially important leave it as it is.
-        Do not make up anything that did not exist in the original message. You are to reduce the original message a little bit.
-        Ommit sayings like 'Here is summarized text:', just give the summarized text.
-        Avoid any text formatting, just plain text.
-        """
-        summarizer_prompt = ChatPromptTemplate.from_template(prompt_template)
-        chain = summarizer_prompt | self.llm
-        response = chain.invoke({
-            "history": self.history[-1]
-        })
-        summary = response.content.strip()
-        state.messages = AIMessage(content=f"{self.name} said: {summary}")
-        return state
+        logger.info(f"{self.name} generated a response")
+        response = AIMessage(f"{self.name} said: {response.content}")
+        return {
+            "history_patch": response
+        }
     
-    def run(self, topic: str, messages: Annotated[List[BaseMessage], add_messages]):
-        init_state = AgentState(
-            topic=topic,
-            messages=messages
-        )
-        result = self.graph.invoke(init_state)
-        return result
+class Orchestrator(BaseAgent):
+
+    async def run(self, state:ChatState):
+        logger.info(f"{self.name} called to the debates")
+        if state.replices_counter == 10:
+            logger.warning(f"Turn limit ({state.replices_counter}) reached. Forcing FINISH.")
+            return {"next_speaker": "finish"}
+        response: AIMessage = await self.chain.ainvoke({
+            "debators": state.debators,
+            "history": state.history,
+            "recent_history": state.recent_history,
+            "last_speaker": state.last_speaker
+        })
+        logger.info(f"{self.name} generated a response")
+        cleaned_response = response.content.strip().lower()
+        options = [debator for debator in state.debators if debator != state.last_speaker]
+        valid_options = options + ["finish"]
+        if cleaned_response not in valid_options:
+            return {
+                "next_speaker": valid_options[0]
+                }
+        logger.info(f"Next speaker is: {cleaned_response}")
+        return {
+            "next_speaker": cleaned_response
+        }
+
+class HistoryManager(BaseAgent):
+
+    def __init__(self, name:str, history_size:int, model:str="openai/gpt-oss-120b"):
+        super().__init__(name, model)
+        self.history_size = history_size
+
+    async def run(self, state: ChatState):
+        logger.info(f"{self.name} called to manage history")
+        
+        new_message = state.history_patch
+        if not new_message:
+            return {}
+
+        current_window = state.recent_history
+
+        new_window = current_window + [new_message]
+
+        if len(new_window) > self.history_size:
+            new_window = new_window[-self.history_size:]
+        
+        return {
+            "history": [new_message], 
+            "recent_history": new_window, 
+            "history_patch": None,
+            "replices_counter": state.replices_counter + 1
+        }
+
+class Judge(BaseAgent):
+
+    async def run(self, state:ChatState):
+        logger.info(f"{self.name} called to the debates")
+        response: AIMessage = await self.chain.ainvoke({
+            "history": state.history
+        })
+        logger.info(f"{self.name} generated a response")
+        return {
+            "judge_decision": response
+        }
