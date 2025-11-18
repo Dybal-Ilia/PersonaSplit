e0dd3504659c10fc345eb9a424e4972bc65efb0a
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-18 15:57:20 +0100

    refactor: remove HistoryManager from GraphFactory and simplify graph structure

--- src/core/agents/orchestration.py
+++ src/core/agents/orchestration.py
@@ -1,18 +1,13 @@
-from langchain.messages import AIMessage
 from langgraph.graph import StateGraph, END, START
-from langchain.tools import tool
-from langchain_core.prompts import ChatPromptTemplate
 from src.utils.logger import logger
-from src.utils.loaders import load_yaml
 from src.core.schemas.state import ChatState
-from src.core.agents.agent import Orchestrator, HistoryManager, Judge, Persona
+from src.core.agents.agent import Orchestrator, Judge, Persona
 from typing import List
 
 class GraphFactory:
 
     def __init__(self, agents_list: List[str]):
         self.orchestrator = Orchestrator(name="orchestrator")
-        self.history_manager = HistoryManager(name="history_manager", history_size=4)
         self.judge = Judge(name="judge")
         self.agents_list = agents_list
 
@@ -20,14 +15,12 @@ class GraphFactory:
     def build_graph(self):
         graph = StateGraph(ChatState)
         graph.add_node("Orchestrator", self.orchestrator.run)
-        graph.add_node("HistoryManager", self.history_manager.run)
         graph.add_node("Judge", self.judge.run)
         graph.add_edge(START, "Orchestrator")
         for agent_name in self.agents_list:
             agent = Persona(name=agent_name)
             graph.add_node(agent_name, agent.run)
-            graph.add_edge(agent_name, "HistoryManager")
-        graph.add_edge("HistoryManager", "Orchestrator")       
+            graph.add_edge(agent_name, "Orchestrator")
         conditional_map = {name: name for name in self.agents_list}
         conditional_map["finish"] = "Judge"
         graph.add_conditional_edges(
